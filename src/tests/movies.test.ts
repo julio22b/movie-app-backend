016f1563bf6cead25ff0def5b4a9fa9529267600
Julio Bermudez <jbermudezfc90@gmail.com>
2020-07-27 12:54:00 -0400

    movies tests pass

---
+++ src/tests/movies.test.ts
@@ -0,0 +1,86 @@
+import supertest from 'supertest';
+import app from '..';
+import { connect, closeDatabase, clearDatabase } from '../mongoConfigTesting';
+import Movie, { IMovie } from '../models/Movie';
+const api = supertest(app);
+const jsonRegex = /application\/json/;
+const baseUrl = '/api/movies';
+
+interface IMovieBase {
+    title: IMovie['title'];
+    year: IMovie['year'];
+    synopsis: IMovie['synopsis'];
+    ratings: IMovie['ratings'];
+    poster: IMovie['poster'];
+    likes?: IMovie['likes'];
+}
+
+beforeAll(async () => {
+    await connect();
+});
+
+describe.only('add and update movie documents', () => {
+    beforeEach(async () => {
+        await clearDatabase();
+        const newMovie: IMovieBase = {
+            title: 'Phantom Thread',
+            year: '2017',
+            synopsis: 'Crazy people',
+            ratings: [5, 5],
+            poster: 'POSTER URL STRING HERE',
+            likes: 10,
+        };
+        await api.post(`${baseUrl}/create`).send(newMovie);
+    });
+
+    test('should create a new movie instance', async () => {
+        const moviesAtStart = await api.get(`${baseUrl}/all`);
+
+        const newMovie: IMovieBase = {
+            title: 'Arrival',
+            year: '2016',
+            synopsis: 'Funny aliens talk to humans',
+            ratings: [5, 5, 5, 4.5],
+            poster: 'url',
+        };
+        await api
+            .post(`${baseUrl}/create`)
+            .send(newMovie)
+            .expect(200)
+            .expect('Content-Type', jsonRegex);
+
+        const moviesAtEnd = await api.get(`${baseUrl}/all`);
+        expect(moviesAtEnd.body).toHaveLength(moviesAtStart.body.length + 1);
+        const movieTitles = moviesAtEnd.body.map((movie) => movie.title);
+        expect(movieTitles).toContain('Arrival');
+    });
+
+    test('should update a movie instance likes', async () => {
+        const beforeLikingMovie = await Movie.findOne({ title: 'Phantom Thread' });
+
+        await api
+            .put(`${baseUrl}/${beforeLikingMovie?._id}/like`)
+            .expect(200)
+            .expect('Content-Type', jsonRegex);
+
+        const afterLikingMovie = await Movie.findOne({ title: 'Phantom Thread' });
+        expect(afterLikingMovie?.likes).toBe(beforeLikingMovie?.likes + 1);
+    });
+
+    test('should update movie instance ratings', async () => {
+        const beforeRatingMovie = await Movie.findOne({ title: 'Phantom Thread' });
+
+        await api
+            .put(`${baseUrl}/${beforeRatingMovie?._id}/rate`)
+            .send({ rating: 4 })
+            .expect(200)
+            .expect('Content-Type', jsonRegex);
+
+        const afterRatingMovie = await Movie.findOne({ title: 'Phantom Thread' });
+        expect(afterRatingMovie?.ratings).toContain(4);
+    });
+});
+
+afterAll(async () => {
+    await closeDatabase();
+});
