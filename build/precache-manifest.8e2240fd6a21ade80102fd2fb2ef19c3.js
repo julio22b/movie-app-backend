5fbde0e64e78634017374c0950a279dab2fe87cd
Julio Bermudez <jbermudezfc90@gmail.com>
2020-08-22 15:11:04 -0400

    add build

---
+++ build/precache-manifest.8e2240fd6a21ade80102fd2fb2ef19c3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7242749872cc6ebce3fb3ccc9b394b0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "76309de1c802f8d75060",
+    "url": "/static/css/main.84470b3f.chunk.css"
+  },
+  {
+    "revision": "8e153db51d05a3f775c0",
+    "url": "/static/js/2.632607ab.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.632607ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76309de1c802f8d75060",
+    "url": "/static/js/main.2a95dc5a.chunk.js"
+  },
+  {
+    "revision": "1bdef57f2972ca9286dd",
+    "url": "/static/js/runtime-main.202a70e3.js"
+  },
+  {
+    "revision": "e915ddc08f412786b3badc0c56e02057",
+    "url": "/static/media/avatar.e915ddc0.webp"
+  },
+  {
+    "revision": "ec5bc1b3148ca464a3a3e17af89c3ead",
+    "url": "/static/media/github-brands.ec5bc1b3.svg"
+  }
+]);
