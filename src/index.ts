9672e144dc0bbea7e6ad530cacc31698b4297181
Julio Bermudez <jbermudezfc90@gmail.com>
2020-08-23 15:49:19 -0400

    change build script

--- src/index.ts
+++ src/index.ts
@@ -89,7 +89,7 @@ app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
  *                              Serve front-end content
  ***********************************************************************************/
 
-app.use(express.static(path.join(__dirname, '../build')));
+app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
